c43b30253e5ffde2d5f30fff19eb4972f0190cfb
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-09-26 15:14:35 +0530

    Initial commit

---
+++ src/components/screens/ProjectsManagement.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectsManagement = () => {
+  return <div>Your projects</div>;
+};
+
+export default ProjectsManagement;
---
+++ src/tests/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import App from "../components/App.component";
+
+test("renders learn react link", () => {
+  return;
+});
